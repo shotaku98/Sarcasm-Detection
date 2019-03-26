5c04b88feb8c566218206f635b21bcfc01409ef1
shotaku98 <37736680+shotaku98@users.noreply.github.com>
2019-03-26 12:04:50 +0530

    Adding Documentation

--- SarcasmDetect.py
+++ SarcasmDetect.py
@@ -13,10 +13,17 @@ from keras.models import Model, Sequential
 max_features = 10000
 maxlen = 25
 embedding_size = 200
+#PRE PROCESSING DATA FOR DEEP LEARNING
 
+"""
+This class allows to vectorize a text corpus, by turning each text into either a sequence of integers 
+"""
 tokenizer = Tokenizer(num_words=max_features)
+#Updates internal vocabulary based on a list of texts. This method creates the vocabulary index based on word frequency.
 tokenizer.fit_on_texts(list(df['headline']))
+#ransforms each text in texts to a sequence of integers. So it basically takes each word in the text and replaces it with its corresponding integer value from the word_index dictionary.
 X = tokenizer.texts_to_sequences(df['headline'])
+#Pads data to fit the input format
 X = pad_sequences(X, maxlen = maxlen)
 y = df['is_sarcastic']
 
